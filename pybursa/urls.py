66dc67df7eafedd527a4b55b5783c2ce823e2416
ud3p <ud3p@mail.ru>
2015-11-18 11:38:15 +0200

    v4_static+url

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -5,8 +5,8 @@ from polls.views import hello, contact, student, best
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^$', hello),
-	url(r'^contact/$', contact),
-	url(r'^student_list/$', student),
-	url(r'^student_detail/$', best),
+	url(r'^$', hello, name='index'),
+	url(r'^contact/$', contact, name='contact'),
+	url(r'^student_list/$', student, name='student_list'),
+	url(r'^student_detail/$', best, name='student_detail'),
 )
