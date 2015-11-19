cf5fc62a637fd11f39bea346dcbc909f5dc1ad89
ud3p <ud3p@mail.ru>
2015-11-19 17:02:29 +0200

    v5_quadratic

--- quadratic/views.py
+++ quadratic/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def quadratic_results(request):
+	#return HttpResponse("Hello")
+	a = request.GET['a']
+	print a
 	return render(request, 'results.html')
+
