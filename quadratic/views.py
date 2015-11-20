3ac1731c2be6f74f2cc6c5ed9c468543bce103ff
ud3p <ud3p@mail.ru>
2015-11-20 12:06:04 +0200

    v5_hold

--- quadratic/views.py
+++ quadratic/views.py
@@ -7,33 +7,56 @@ def quadratic_results(request):
 	return render(request, 'results.html')
 
 def calculate(request):
-	yravnenie = u"Квадратное уравнение a*x*x + b*x + c = 0"
+	yravnenie = "Квадратное уравнение a*x*x + b*x + c = 0"
 	def get_discr(a, b, c):
 		d = b**2 - 4*a*c
 		return d
 	
 	r = request.GET
-	a = int(r['a'])
-	b = int(r['b'])
-	c = int(r['c'])
-	dictionary = {'a': a, 'b': b, 'c': c}
+	print r
+	dictionary = {'a': r['a'], 'b': r['b'], 'c': r['c']}
+	error_mess1 = "коэффициент не целое число"
+	error_mess2 = "коэффициент не определен"
+	try:
+		a = int(r['a'])
+		b = int(r['b'])
+		c = int(r['c'])
+		
+	except (ValueError, UnboundLocalError):
+		if dictionary['a'].isalpha():
+			return render(request, 'results.html', {'dictionary': dictionary, 'error_message_a': error_mess1})
+		elif dictionary['b'].isalpha():
+			return render(request, 'results.html', {'dictionary': dictionary, 'error_message_b': error_mess1})
+		elif dictionary['c'].isalpha():
+			return render(request, 'results.html', {'dictionary': dictionary, 'error_message_c': error_mess1})
+		elif dictionary['a']=='':
+			return render(request, 'results.html', {'dictionary': dictionary, 'error_message_a': error_mess2})
+		elif dictionary['b']=='':
+			return render(request, 'results.html', {'dictionary': dictionary, 'error_message_b': error_mess2})
+		elif dictionary['c']=='':
+			return render(request, 'results.html', {'dictionary': dictionary, 'error_message_c': error_mess2})
+	
+		
+
+	
 
 	d = get_discr(a, b, c)
-	outp_d = u"Дискриминант: %s" %d
+	outp_d = "Дискриминант: %s" %d
 	if d < 0:
-		discr = {'discr':u"Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."}
+		outp = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
+		discr = {'discr': outp, 'd': outp_d, 'dictionary': dictionary}
 		return render(request, 'results.html', discr)
 	
 	elif d == 0:
 		x = -b / 2*a
-		outp = u"Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %s" % float(x)
+		outp = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %s" % float(x)
 		discr = {'discr': outp, 'd': outp_d, 'dictionary': dictionary}
 		return render(request, 'results.html', discr)
 
 	else:
 		x1 = (-b + d**(1/2.0)) / 2*a
 		x2 = (-b - d**(1/2.0)) / 2*a
-		outp = u"Квадратное уравнение имеет два действительных корня: x1 = %s x2 = %s" % (float(x1), float(x2))
+		outp = "Квадратное уравнение имеет два действительных корня: x1 = %s x2 = %s" % (float(x1), float(x2))
 		discr = {'discr': outp, 'd': outp_d, 'dictionary': dictionary}
 		return render(request, 'results.html', discr)
 	
