a9e11d9448307eda6f2f39edd90e884dcb421837
ud3p <ud3p@mail.ru>
2015-12-02 23:54:48 +0200

    test

--- quadratic/views.py
+++ quadratic/views.py
@@ -86,16 +86,17 @@ def quadratic_results(request):
 			a = form.cleaned_data['a']
 			b = form.cleaned_data['b']
 			c = form.cleaned_data['c']
-			d = get_discr(a, b, c)
+
+			d = b ** 2 - 4 * a * c
 
 			if d < 0:
 				result_message = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
 			elif d == 0:
-				x = get_eq_root(a, b, d)
+				x = (-b + d**(1/2.0)) / 2*a
 				result_message = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = {}" .format(x)
 			else:
-				x1 = get_eq_root(a, b, d)
-				x2 = get_eq_root(a, b, d, order=2)
+				x1 = (-b + d**(1/2.0)) / 2*a
+				x2 = (-b - d**(1/2.0)) / 2*a
 				result_message = "Квадратное уравнение имеет два действительных корня: x1 = {}, x2 = {}" .format(x1, x2)
 		
 			context.update({'d': 'Дискриминант: %d' %d, 'result_message': result_message})
