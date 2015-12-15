5eec7b9443e88a9a9c7bf7142e8259a7facb34e1
ng <ud3p@mail.ru>
2015-12-15 23:32:30 +0200

    KZ_11.2

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,34 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
+from students.models import Student
+from courses.models import Course
+from django.test import Client
+import os.path
 
+def test_insert_course():
+    new_student = Course.objects.create(
+                            name='Python/Django',
+                            short_description='Практический курс по изучению современного языка программирования Python и фреимворка Django.')
+    return new_student
 # Create your tests here.
+class CoursesListTest(TestCase):
+    def test_new_course(self):
+        test_insert_course()
+        self.assertEqual(Course.objects.all().count(), 1)
+
+    def test_page_c(self):
+        client = Client()
+        test_insert_course()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'PYTHON/DJANGO')
+
+class CoursesDetailTest(TestCase):
+    def test_detail_page_c(self):
+        client = Client()
+        response = client.get('/courses/1/')
+        self.assertEqual(response.status_code, 404)
+        test_insert_course()
+        response = client.get('/courses/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Python/Django')
--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,39 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
+from students.models import Student
+from courses.models import Course
+from django.test import Client
+import os.path
 
+def test_insert_student():
+    new_student = Student.objects.create(
+                            name='Test',
+                            surname='Test',
+                            date_of_birth='1987-02-02',
+                            email='test@test.com',
+                            phone='099-999-99-99',
+                            address='ул. Пушкина, д. 57, кв. 137',
+                            skype='test',)
+    return new_student
 # Create your tests here.
+class StudentsListTest(TestCase):
+    def test_new_student(self):
+        test_insert_student()
+        self.assertEqual(Student.objects.all().count(), 1)
+
+    def test_page_s(self):
+        client = Client()
+        test_insert_student()
+        response = client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'ул. Пушкина, д. 57, кв. 137')
+
+class StudentsDetailTest(TestCase):
+    def test_detail_page_s(self):
+        client = Client()
+        response = client.get('/students/1/')
+        self.assertEqual(response.status_code, 404)
+        test_insert_student()
+        response = client.get('/students/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'test@test.com')
