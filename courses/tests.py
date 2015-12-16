27598059ae7e1317cbc95d6d9ac7d3db6b1d9ad5
ud3p <ud3p@mail.ru>
2015-12-16 16:46:10 +0200

    KZ_11.2_add

--- courses/tests.py
+++ courses/tests.py
@@ -17,21 +17,6 @@ def insert_course():
     new_user_3 = User.objects.create(
                             username='user_3')
 
-    new_course_1 = Course.objects.create(
-                            name='Python/Django',
-                            short_description='Практический курс',
-                            description='Практический курс по изучению современного языка программирования Python и фреимворка Django.')
-
-    new_course_2 = Course.objects.create(
-                            name='JavaScript',
-                            short_description='Базовый JavaScript',
-                            description='Базовый JavaScript, клиентский JavaScript, работа с HTML5, написание плагинов jQuery, использование шаблонов, введение в Backbone JS.')
-
-    new_course_3 = Course.objects.create(
-                            name='Web-дизайн',
-                            short_description='Освоите Photoshop и Illustrator.',
-                            description='Освоите на профессиональному уровне работу с такими программами, как Photoshop и Illustrator.')
-
     new_coach_1 = Coach.objects.create(
                             user=new_user_1,
 	                        date_of_birth='1985-11-26',
@@ -40,42 +25,126 @@ def insert_course():
 	                        address='ул. Садовая, дом 75, квартира 156',
 	                        skype='user_1',
 	                        description='Руководитель группы разработки веб-проектов')
+    
+    new_coach_2 = Coach.objects.create(
+                            user=new_user_2,
+	                        date_of_birth='1986-11-26',
+	                        gender='F',
+	                        phone='(050) 847-73-38',
+	                        address='ул. Садовая, дом 75, квартира 157',
+	                        skype='user_2',
+	                        description='Руководитель группы разработки веб-проектов')
+    
+    new_coach_3 = Coach.objects.create(
+                            user=new_user_3,
+	                        date_of_birth='1987-11-26',
+	                        gender='M',
+	                        phone='(050) 847-73-39',
+	                        address='ул. Садовая, дом 75, квартира 158',
+	                        skype='user_3',
+	                        description='Руководитель группы разработки веб-проектов')
+
+
+    new_course_1 = Course.objects.create(
+                            name='Python/Django',
+                            short_description='Практический курс',
+                            description='Практический курс по изучению современного языка программирования Python и фреимворка Django.',
+                            coach=new_coach_1,
+                            assistant=new_coach_3)
+
+    new_course_2 = Course.objects.create(
+                            name='JavaScript',
+                            short_description='Базовый JavaScript',
+                            description='Базовый JavaScript, клиентский JavaScript, работа с HTML5, написание плагинов jQuery, использование шаблонов, введение в Backbone JS.',
+                            coach=new_coach_1,
+                            assistant=new_coach_2)
+
+    new_course_3 = Course.objects.create(
+                            name='Web-дизайн',
+                            short_description='Освоите Photoshop и Illustrator.',
+                            description='Освоите на профессиональному уровне работу с такими программами, как Photoshop и Illustrator.',
+                            coach=new_coach_3,
+                            assistant=new_coach_2)
 
 
   
 
 # Create your tests here.
 class CoursesListTest(TestCase):
-    def test_new_course(self):
+    def setUp(self):
+        self.client = Client()
         insert_course()
+
+    def test_new_course(self):
+        #insert_course()
         self.assertEqual(Course.objects.all().count(), 3)
 
     def test_page_c(self):
-        client = Client()
-        insert_course()
-        response = client.get('/')
+        #client = Client()
+        #insert_course()
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'PYTHON/DJANGO')
         self.assertTemplateUsed(response, 'index.html')
+    
+    def test_link_add_edit_remove_course(self):
+        #client = Client()
+        #insert_course()
+        response = self.client.get('/')
+        for i in range(1, 4):
+            self.assertContains(response, '/courses/add/')
+            self.assertContains(response, '/courses/edit/{}/'.format(i))
+            self.assertContains(response, '/courses/remove/{}/'.format(i))
+    
+    def test_check_valid_add(self):
+        #client = Client()
+        #insert_course()
+        response = self.client.get('/courses/add/')
+        self.assertContains(response, 'Name')
+        self.assertContains(response, 'Short description')
+        self.assertContains(response, 'Description')
+        self.assertContains(response, 'Coach')
+        self.assertContains(response, 'Assistant')
+    
+    def test_check_valid_edit(self):
+        #client = Client()
+        #insert_course()
+        response = self.client.get('/courses/edit/1/')
+        self.assertContains(response, 'user_1')
+        self.assertContains(response, 'user_3')
+    
+    def test_check_valid_remove(self):
+        #client = Client()
+        #insert_course()
+        for i in range(1, 4):
+            response = self.client.post('/courses/remove/{}/'.format(i))
+            self.assertEqual(response.status_code, 302)
+            response = self.client.get('/courses/remove/{}/'.format(i))
+            self.assertEqual(response.status_code, 404)
+    
 
 class CoursesDetailTest(TestCase):
-    def test_detail_page_c(self):
-        client = Client()
-        response = client.get('/courses/1/')
-        self.assertEqual(response.status_code, 404)
+    def setUp(self):
+        self.client = Client()
         insert_course()
-        response = client.get('/courses/1/')
+
+    def test_detail_page_c(self):
+        #client = Client()
+        #response = self.client.get('/courses/1/')
+        #self.assertEqual(response.status_code, 404)
+        #insert_course()
+        response = self.client.get('/courses/1/')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Практический курс')
         self.assertTemplateUsed(response, 'courses/detail.html')
 
     def test_link_student(self):
-        client = Client()
-        response = client.get('/students/', {'course_id': 1})
+        #client = Client()
+        response = self.client.get('/students/', {'course_id': 1})
         self.assertEqual(response.status_code, 200)
 
     def test_coach(self):
-        client = Client()
-        insert_course()
-        response = client.get('/coaches/1/')
+        #client = Client()
+        #insert_course()
+        response = self.client.get('/coaches/1/')
         self.assertEqual(response.status_code, 200)
