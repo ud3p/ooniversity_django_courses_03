bd951e5ec1bf696275d485abf167e5641c8a65a3
ng <ud3p@mail.ru>
2015-12-19 06:41:25 +0200

    deploy

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '+$+ikpr%#30a3zcv1e!21mkvwlubey&w$#*dp2-^!qnr=x$0ww'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = False
+#DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
-
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
